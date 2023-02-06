cdb939f42c54fa3cb29e414df89cb3f7caa961a5
unknown <huynhlong442000@gmail.com>
2023-02-07 00:20:53 +0700

    uploaf2

--- src/App.js
+++ src/App.js
@@ -11,24 +11,28 @@ import Contact from './pages/Contact';
 import Home from './pages/Home';
 
 function App() {
+  const getEmail = (email) => {
+    console.log(email);
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
-{/* ----------------------------  */}
+        {/* ----------------------------  */}
         <Header />
-{/* ----------------------------  */}
+        {/* ----------------------------  */}
 
         <Routes>
           <Route path="/About" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Contact" element={<Contact 
+          getEmail={getEmail} />} />
           <Route path="/Info" element={<Info />} />
           <Route path="/" element={<Home />} />
         </Routes>
-
       </BrowserRouter>
-{/* ------------------------- */}
+      {/* ------------------------- */}
       <Footer />
-{/* ----------------------------  */}
+      {/* ----------------------------  */}
     </div>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,48 @@
-import React from 'react'
-import '../style/pages/Contact.scss'
+import React from 'react';
+import '../style/pages/Contact.scss';
+import Logo from '../assets/multiplePizzas.jpeg';
+import { useState } from 'react';
 
-function Contact() {
+function Contact(props) {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(null);
+
+  const isValidEmail = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const handleChange = (event) => {
+    if (event.target.value === '' || isValidEmail(event.target.value)) {
+      setError(null);
+      setEmail(event.target.value);
+    } else {
+      setError('Email is invalid');
+    }
+
+    setEmail(event.target.value);
+  };
+
+  const handleCLick = (e) => {
+    e.preventDefault();
+    props.getEmail(email);
+  };
+
   return (
-    <div>
-      Contact
+    <div className="contact" style={{ backgroundImage: `url( ${Logo})` }}>
+      <span className="title">Email for us</span>
+      <span className="title-email">PizzaHut@gmail.com</span>
+
+      <form className="contact-form">
+        <input
+          type="email"
+          placeholder="enter your email right here"
+          value={email}
+          onChange={handleChange}
+        />
+        <button onClick={handleCLick}>Submit</button>
+        {error && <h2 style={{ color: 'red' }}>{error}</h2>}
+      </form>
     </div>
-  )
+  );
 }
-export default Contact
+export default Contact;
